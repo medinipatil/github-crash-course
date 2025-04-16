613953264b969211a09196437d58830508b85002
Medini Patil <patilmedini333@gmail.com>
2025-04-17 00:49:29 +0530

    Added second version

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const age = 24;
-const name = 'Medini Patil'
-const dob = '03/06/2002'    
+const name1 = 'Medini Patil'
+const dob = '03/06/2002'
+const fatherName = 'Vikas'    
 
 
 function sayName(){
-    console.log('My Name is' +name)
+    console.log('My Name is' +name1)
+}
+
+function isAbove(){
+    return age >= 18
 }
